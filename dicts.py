6a9eabf7da158c545724825335f3759930d1d71d
Ludmila <ludaarozhkovskaya@gmail.com>
2025-05-30 11:53:34 +0300

    Parser

---
+++ dicts.py
@@ -0,0 +1,13 @@
+
+class HtmlDicts:
+    base_dir = r"C:/Users/ludaa/Desktop/Работа/парсер/done_html/"
+
+    html_dicts = {
+        0: f"{base_dir}conneection.html",
+        1: f"{base_dir}gsm.html",
+        2: f"{base_dir}parts.html",
+        3: f"{base_dir}position.html",
+        4: f"{base_dir}positionRods.html",
+        5: f"{base_dir}seam.html"
+    }
+    
